612e525b8b6f17297708bbf8dbc5ef66c838450e
LeaderQiu <316894551@qq.com>
2015-06-06 11:38:41 +0800

    新增DetailCell.xib

---
+++ ZhonglieApp/ZhonglieApp/View/PXDetailCell.h
@@ -0,0 +1,13 @@
+//
+//  PXDetailCell.h
+//  ZhonglieApp
+//
+//  Created by 邱思雨 on 15/6/6.
+//  Copyright (c) 2015年 PX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PXDetailCell : UITableViewCell
+
+@end
---
+++ ZhonglieApp/ZhonglieApp/View/PXDetailCell.m
@@ -0,0 +1,23 @@
+//
+//  PXDetailCell.m
+//  ZhonglieApp
+//
+//  Created by 邱思雨 on 15/6/6.
+//  Copyright (c) 2015年 PX. All rights reserved.
+//
+
+#import "PXDetailCell.h"
+
+@implementation PXDetailCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
