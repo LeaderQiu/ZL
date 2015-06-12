8fed7bd5a057dbe1765147723ede7f08c1c03ca7
LeaderQiu <316894551@qq.com>
2015-06-12 18:22:43 +0800

    我恨TableView!

--- ZhonglieApp/ZhonglieApp/PXAddRunameViewController.m
+++ ZhonglieApp/ZhonglieApp/PXAddRunameViewController.m
@@ -8,9 +8,10 @@
 
 #import "PXAddRunameViewController.h"
 #import "UIBarButtonItem+Extension.h"
+#import "UIColor+SYExtension.h"
 
 
-@interface PXAddRunameViewController ()<UITextFieldDelegate>
+@interface PXAddRunameViewController ()<UITextFieldDelegate,UITableViewDataSource,UITableViewDelegate>
 
 @property(nonatomic,strong) UITextField *TextField;
 
@@ -30,13 +31,28 @@
     [self setupHeaderV];
     
     [self setupScrollV];
+    
+    [self setupTableV];
 }
 
 
+//加载tableView内容
+-(void)setupTableV
+{
+    UITableView *TableV = [[UITableView alloc]initWithFrame:CGRectMake(0, 114, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height) style:UITableViewStyleGrouped];
+    
+    TableV.backgroundColor = [UIColor colorWithRGB:0xececec];
+    
+    TableV.delegate = self;
+    TableV.dataSource = self;
+    
+    [self.view addSubview:TableV];
+    
+}
 //加载ScrollView内容
 -(void)setupScrollV
 {
-
+    
 }
 
 //加载头部搜索栏
@@ -44,7 +60,7 @@
 {
     UIView *HeaderV = [[UIView alloc]initWithFrame:CGRectMake(0, 64, self.view.bounds.size.width, 50)];
     
-    HeaderV.backgroundColor = [UIColor grayColor];
+    HeaderV.backgroundColor = [UIColor whiteColor];
     
     UITextField *TextField = [[UITextField alloc]initWithFrame:CGRectMake(10, 10, 300, 33)];
     
@@ -52,7 +68,7 @@
     
     TextField.delegate = self;
     
-    TextField.background = [UIImage imageNamed:@"searchBarBack"];
+    TextField.background = [UIImage imageNamed:@"searchBar"];
     
     [self setupTextFiel];
     
@@ -73,7 +89,7 @@
     self.TextField.autocapitalizationType = UITextAutocapitalizationTypeNone;
     self.TextField.returnKeyType = UIReturnKeySearch;
     
-    self.TextField.background = [UIImage imageNamed:@"searchBarBack"];
+    self.TextField.background = [UIImage imageNamed:@"searchBar"];
     
     
     UIImageView *Searchimage=[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"查找职位"]];
@@ -112,5 +128,129 @@
 {
     [self.navigationController popToRootViewControllerAnimated:YES];
 }
+
+
+#pragma mark - Table代理和数据源
+
+//每个cell
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellId = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellId];
+    
+    if (cell == nil) {
+        
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellId];
+        
+        int section = indexPath.section;
+        int row = indexPath.row;
+        
+        switch (section) {
+            case 0:
+                if (row == 0) {
+                    cell.textLabel.text = @"姓名：";
+                }
+                if (row == 1) {
+                    cell.textLabel.text = @"性别：";
+                }
+                if (row == 2) {
+                    cell.textLabel.text = @"年龄：";
+                }
+                if (row == 3) {
+                    cell.textLabel.text = @"移动电话：";
+                }
+                break;
+            case 1:
+                if (row == 0) {
+                    cell.textLabel.text = @"学校：";
+                }
+                if (row == 1) {
+                    cell.textLabel.text = @"专业：";
+                }
+                if (row == 2) {
+                    cell.textLabel.text = @"学历：";
+                }
+                break;
+            case 2:
+                if (row == 0) {
+                    cell.textLabel.text = @"公司：";
+                }
+                if (row == 1) {
+                    cell.textLabel.text = @"职位：";
+                }
+                if (row == 2) {
+                    cell.textLabel.text = @"在职时间：";
+                }
+                if (row == 3) {
+                    cell.textLabel.text = @"工作业绩及推荐内容：";
+                }
+            case 3:
+                if (row == 0) {
+                    cell.textLabel.text = @"姓名+职位+时间";
+                }
+                
+            default:
+                break;
+        }
+    }
+    
+    
+
+    return cell;
+
+}
+
+//头部标题
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return @"个人信息";
+    }
+    if (section == 1) {
+        return @"教育经历";
+    }
+    if (section == 2) {
+        return @"工作经历";
+    }
+    if (section == 3) {
+        return @"简历名称";
+    }
+
+    return @"个人信息";
+}
+
+//组头高度
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 40;
+}
+
+//每组几个cell
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    
+    if (section == 0) {
+        return 4;
+    }
+    if (section == 1) {
+        return 3;
+    }
+    if (section == 2) {
+        return 4;
+    }
+    if (section == 3) {
+        return 1;
+    }
+    return 3;
+}
+//几个组
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 4;
+}
+
+
+
+
 
 @end
