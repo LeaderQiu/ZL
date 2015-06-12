3275af428b34c2148644eaae6e6655afc1b839a3
LeaderQiu <316894551@qq.com>
2015-06-12 10:26:38 +0800

    创建密码找回文件

---
+++ ZhonglieApp/ZhonglieApp/PXMiMaViewController1.m
@@ -0,0 +1,37 @@
+//
+//  PXMiMaViewController1.m
+//  ZhonglieApp
+//
+//  Created by 邱思雨 on 15/6/12.
+//  Copyright (c) 2015年 PX. All rights reserved.
+//
+
+#import "PXMiMaViewController1.h"
+
+@interface PXMiMaViewController1 ()
+
+@end
+
+@implementation PXMiMaViewController1
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ ZhonglieApp/ZhonglieApp/PXMiMaViewController2.m
@@ -0,0 +1,37 @@
+//
+//  PXMiMaViewController2.m
+//  ZhonglieApp
+//
+//  Created by 邱思雨 on 15/6/12.
+//  Copyright (c) 2015年 PX. All rights reserved.
+//
+
+#import "PXMiMaViewController2.h"
+
+@interface PXMiMaViewController2 ()
+
+@end
+
+@implementation PXMiMaViewController2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
