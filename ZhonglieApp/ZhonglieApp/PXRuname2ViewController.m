c1d4a75fcb3a6c1a7ea2d277173e83d276982d54
LeaderQiu <316894551@qq.com>
2015-06-24 15:55:22 +0800

    准备完善简历页面

--- ZhonglieApp/ZhonglieApp/PXRuname2ViewController.m
+++ ZhonglieApp/ZhonglieApp/PXRuname2ViewController.m
@@ -12,6 +12,7 @@
 #import "PXDetailViewController.h"
 #import "PXSuccessViewController.h"
 #import "Masonry.h"
+#import "PXWanShanViewController.h"
 
 
 
@@ -167,52 +168,10 @@
 //点击推荐跳转到成功界面
 -(void)TuiJianSuccess
 {
-    PXSuccessViewController *SuccessVC = [[PXSuccessViewController alloc]init];
+//    PXSuccessViewController *SuccessVC = [[PXSuccessViewController alloc]init];
+    PXWanShanViewController *VC = [[PXWanShanViewController alloc]init];
     
-    [self.navigationController pushViewController:SuccessVC animated:YES];
+    [self.navigationController pushViewController:VC animated:YES];
 }
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
- } else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
-
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
 
 @end
